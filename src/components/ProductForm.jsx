cea9fe6af72e480222376f95c5d7278055691179
rengoku-senpei <benmannathara1999@gmail.com>
2022-12-08 17:50:55 +0530

    Final Commit(08/12/22)

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addToDatabase } from '../actions';
 import { fetchData } from '../slice/apiFetchSlice';
 
